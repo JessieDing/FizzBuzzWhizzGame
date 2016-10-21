aeee8c4f5ebc2630fcf10b80562eed60beb9e75f
Jessie Ding <nathalie.ding@gmail.com>
2016-10-21 19:29:33 +0800

    Refactor

--- src/main/java/com/jessie/DefaultFizzHandler.java
+++ src/main/java/com/jessie/DefaultFizzHandler.java
@@ -7,10 +7,13 @@ public class DefaultFizzHandler extends BaseHandler {
         int fizzNumber = specialNumber.getFizzNumber();
         int ten = 10;
         int hundred = 100;
+        int thousand = 1000;
 
         if (number % ten == fizzNumber) {  //判断个位是否含有3
             result = "Fizz";
         } else if (number % hundred / ten == fizzNumber) {    // 判断十位是否含3；“/”除，先对100取余，后除以10，int是整型。注意余数定义
+            result = "Fizz";
+        }else if (number%thousand/hundred == fizzNumber){
             result = "Fizz";
         }
         if (!result.equals("Fizz") && this.getNextHandler() != null) {
